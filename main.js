95d1ea5ae00808977d076681c43d382c1e86cfbb
rulo-dev <rulosan77@gmail.com>
2025-07-03 17:17:30 -0600

    segunda parte: botones

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Con estas lineas de código, se agrega un botón que permite al usuario seleccionar una imagen desde su dispositivo.
+Al hacer clic en el botón, se simula un clic en el input de tipo file, lo que abre el diálogo de selección de archivos.
+El input de tipo file está oculto para que no sea visible en la interfaz, pero sigue siendo funcional.
+El usuario puede seleccionar una imagen y luego se puede manejar el evento de cambio para procesar la imagen seleccionada.
+Este código es útil para aplicaciones web donde se necesita que el usuario suba imágenes, como en un formulario o una galería de imágenes.
+
+Pero no es necesario ya que desde el HTML se puede cambiar el input por label y así se puede hacer clic en el label para abrir el input de tipo file.
+Esto permite una mejor experiencia de usuario y un diseño más limpio.
+*/
+
+// const addButton = document.querySelector('#add-image-button');
+// const imageInput = document.querySelector('#image-input');
+
+// addButton.addEventListener('click', () => {
+//   imageInput.click();
+// });
